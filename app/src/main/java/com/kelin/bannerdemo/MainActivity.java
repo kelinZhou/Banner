da6a7774a4011f363c3982d332f82426e88842eb
kelin <kelin410@163.com>
2023-03-15 22:46:07 +0800

    测试SlideShowMoreLayout组件。

--- app/src/main/java/com/kelin/bannerdemo/MainActivity.java
+++ app/src/main/java/com/kelin/bannerdemo/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
